bc9b5c849a3c21408a7c6b1235d4cbbfd76398b0
Izabella Queiroz Cardoso <izabella.cardoso@hiplatform.com>
2020-11-16 18:36:44 -0300

    uploud

--- app/src/main/java/br/com/opeimpacta/ommani/TelaSobreActivity.kt
+++ app/src/main/java/br/com/opeimpacta/ommani/TelaSobreActivity.kt
@@ -3,7 +3,9 @@ package br.com.opeimpacta.ommani
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.net.Uri
 import android.view.MenuItem
+import android.widget.ImageView
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
@@ -15,6 +17,7 @@ import kotlinx.android.synthetic.main.nav_view.*
 import kotlinx.android.synthetic.main.toolbar.*
 
 class TelaSobreActivity : DebugActivity(){
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_sobre)
@@ -26,6 +29,22 @@ class TelaSobreActivity : DebugActivity(){
 
         supportActionBar?.title = "Sobre Nós"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        imgInsta.setImageResource(R.drawable.instagram)
+        imgFace.setImageResource(R.drawable.facebook)
+        imgSite.setImageResource(R.drawable.web)
+
+        imgInsta.setOnClickListener({ val i = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/ommanicristais/?hl=pt-br"))
+            startActivity(i)
+        })
+
+        imgFace.setOnClickListener({ val i = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/izabella.queiroz.79/"))
+            startActivity(i)
+        })
+
+        imgSite.setOnClickListener({ val i = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.ommanicristais.com.br/index.html"))
+            startActivity(i)
+        })
 
         botao_mapa.setOnClickListener { onClickMapa() }
         configuraMenuLateral()
